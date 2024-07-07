1473ad4f61365b158e53e1c69c9203a62b5a9f49
casjay <git-admin@casjaysdev.pro>
2024-07-07 14:23:42 -0400

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/etc/docker/init.d/zz-echoip.sh

--- rootfs/usr/local/etc/docker/init.d/zz-echoip.sh
+++ rootfs/usr/local/etc/docker/init.d/zz-echoip.sh
@@ -365,7 +365,7 @@ __run_start_script() {
           eval env -i HOME="$home" LC_CTYPE="$lc_type" PATH="$path" HOSTNAME="$sysname" USER="${SERVICE_USER:-$RUNAS_USER}" $extra_env sh -c "$cmd_exec" ||
           return 10
       else
-        su_cmd sh -c "$cmd_exec" || eval "$cmd_exec" || return 10
+        su_cmd "$cmd_exec" || eval "$cmd_exec" || return 10
       fi
     fi
   fi
